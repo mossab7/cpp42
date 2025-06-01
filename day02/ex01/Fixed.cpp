90b1c7193330c331ff4c38e54f7785b9ef3eed7f
blackstar <blackstar@blackstar.blackstar>
2025-06-01 17:25:47 -0400

    completed day02/ex01

--- day02/ex01/Fixed.cpp
+++ day02/ex01/Fixed.cpp
@@ -1,5 +1,32 @@
 #include "Fixed.hpp"
 
+const int Fixed::fraction = 8;
+
+float Fixed::toFloat(void) const
+{
+	return (fixed / static_cast<float>(1 << fraction));
+}
+
+int Fixed::toInt(void) const
+{
+	return (fixed / (1 << fraction));
+}
+float ft_roundf(float value)
+{
+	return (value >= 0.0f) ? (int)(value + 0.5f) : (int)(value - 0.5f);
+}
+
+Fixed::Fixed(float f)
+{
+	std::cout << "Float constructor called" << std::endl;
+	fixed = static_cast<int>(ft_roundf(f * (1 << fraction)));
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& ref)
+{
+	os << ref.fixed / static_cast<float>(1 << Fixed::fraction);
+	return os;
+}
 Fixed::Fixed() : fixed(0)
 {
 	std::cout << "Default constructor called" << std::endl;
--- day02/ex01/Fixed.hpp
+++ day02/ex01/Fixed.hpp
@@ -10,26 +10,17 @@ class Fixed
 	int fixed;
 	public :
 		static const int fraction;
-		static int toInt(const std::string &str);
-		static std::string toString(float f);
 		Fixed();
 		Fixed(float f);
 		~Fixed();
 		Fixed(const Fixed &other);
 		Fixed& operator=(const Fixed &other);
-		void operator>>();
+		friend std::ostream& operator<<(std::ostream& os, const Fixed& ref);
+		float toFloat(void) const;
+		int toInt(void) const;
 		int getRawBits(void);
 		void setRawBits(int const raw);
 };
 
-float ft_roundf(float value)
-{
-	return (value >= 0.0f) ? (int)(value + 0.5f) : (int)(value - 0.5f);
-}
-
-Fixed::Fixed(float f)
-{
-	fixed = static_cast<int>(ft_roundf(f * (1 << fraction)));
-}
 
 #endif //FIXED_HPP
