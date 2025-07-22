d44a7404f12fc642ced7a4eed5bc2b1ffc8a38e1
Mossaab Bouhia <mossabbouhia@gmail.com>
2025-07-22 19:57:48 +0100

    day08/ex01 added

---
+++ day08/ex00/easyfind.hpp
@@ -0,0 +1,17 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+
+template <typename Container>
+typename Container::iterator easyfind(Container& container, int val)
+{
+    for (auto iter = container.begin(); iter != container.end(); ++iter)
+    {
+        if (*iter == val)
+            return iter;
+    }
+    return container.end();
+}
+
+
+#endif //EASYFIND_HPP
