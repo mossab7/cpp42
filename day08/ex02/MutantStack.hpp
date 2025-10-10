df796aaed259bd996be84fe8f765dd6bc46c85a1
mossab7 <mossabbouhia@gmail.com>
2025-10-10 15:20:27 -0400

    changes in ex02

--- day08/ex02/MutantStack.hpp
+++ day08/ex02/MutantStack.hpp
@@ -2,30 +2,85 @@
 #define MUTANTSTACK_HPP
 
 #include <stack>
+#include <cstddef>
 #include <deque>
 
 template <typename T>
 class MutantStack : public std::stack<T>
 {
 public:
-	MutantStack() {}
-	~MutantStack() {}
+	MutantStack();
+	MutantStack(const MutantStack &other);
+	~MutantStack();
+
+	MutantStack &operator=(const MutantStack &other);
 
 	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 
-	iterator begin()
-	{
-		return this->c.begin();
-	}
+	iterator begin();
+	iterator end();
+	const_iterator begin() const;
+	const_iterator end() const;
 
-	iterator end()
-	{
-		return this->c.end();
-	}
-	void push_back(T value)
-	{
-		this->push(value);
-	}
+	template <typename F>
+	void push_range(F begin, F end);
+
 };
+
+
+template <typename T>
+MutantStack<T>::MutantStack() {}
+
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack &other)
+	: std::stack<T>(other) {}
+
+template <typename T>
+MutantStack<T>::~MutantStack() {}
+
+template <typename T>
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack<T> &other)
+{
+	if (this != &other)
+		std::stack<T>::operator=(other);
+	return *this;
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin()
+{
+	return this->c.begin();
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end()
+{
+	return this->c.end();
+}
+
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::begin() const
+{
+	return this->c.begin();
+}
+
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::end() const
+{
+	return this->c.end();
+}
+
+template <typename T>
+template <typename F>
+void MutantStack<T>::push_range(F begin, F end)
+{
+	while (begin != end)
+	{
+		this->push(*begin);
+		++begin;
+	}
+
+}
 
 #endif // MUTANTSTACK_HPP
--- day08/ex02/main.cpp
+++ day08/ex02/main.cpp
@@ -1,15 +1,25 @@
 #include "MutantStack.hpp"
 #include <iostream>
+#include <vector>
 #include <stack>
+#include <cstddef>
 int main(void)
 {
-	MutantStack <int>mstack;
-	mstack.push_back(10);
-	mstack.push_back(20);
+	std::vector<int> vec;
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
+	vec.push_back(4);
+	vec.push_back(5);
+	MutantStack<int> mstack;
+	mstack.push_range(vec.begin(), vec.end());
+	mstack.push(6);
+	mstack.push(7);
 	for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); it++)
 	{
 		std::cout << *it << std::endl;
 	}
+	std::cout << "-----------------size: " << mstack.size() << std::endl;
 	std::stack<int> s(mstack);
 	while (!s.empty())
 	{
